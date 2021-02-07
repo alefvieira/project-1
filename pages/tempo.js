6e59da225d91aa974c11c2ebe1bee7f40d69c949
alefvieira <78700687+alefvieira@users.noreply.github.com>
2021-02-07 20:18:19 -0300

    Add files via upload

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,21 +1,25 @@
 function Tempo(props){
+    console.log('> Passando pelo Frontend')
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
     return(
         <div>
             <div>{dynamicDateString}(dinâmico)</div>
+            {/* congela a data ou ovalor no momento que a pag é aberta */}
             <div>{props.staticoDateString} (estatico)</div>
         </div>
     )
 }
 export function getStaticProps(){
+    console.log('> Passando pelo getStaticProps();')
     const staticoDate = new Date();
     const staticoDateString = staticoDate.toGMTString();
     return{
         props:{
             staticoDateString
-        }
+        },
+        revalidate: 1
     }
 
 }
